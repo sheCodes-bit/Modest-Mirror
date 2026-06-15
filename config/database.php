bf4afb313bce95772fe70b38070dc481cd6e1c90
Esha Minahil <eshaminahil1@gmail.com>
2026-06-15 20:33:50 +0500

    fix: secure TiDB SSL connection for production

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
     'engine' => null,
 
     'options' => extension_loaded('pdo_mysql') ? array_filter([
-    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+    PDO::MYSQL_ATTR_SSL_MODE => PDO::MYSQL_ATTR_SSL_MODE_REQUIRED,
 ]) : [],
 ],
 
