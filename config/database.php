ceac41dcafda3df1f6492ad5845097cbb4e48e26
Esha Minahil <eshaminahil1@gmail.com>
2026-06-16 18:48:39 +0500

    Fix TiDB SSL configuration

--- config/database.php
+++ config/database.php
@@ -49,8 +49,8 @@ return [
     'url' => env('DB_URL'),
     'host' => env('DB_HOST', '127.0.0.1'),
     'port' => env('DB_PORT', '3306'),
-    'database' => env('DB_DATABASE', 'forge'),
-    'username' => env('DB_USERNAME', 'forge'),
+    'database' => env('DB_DATABASE', 'laravel'),
+    'username' => env('DB_USERNAME', 'root'),
     'password' => env('DB_PASSWORD', ''),
     'unix_socket' => env('DB_SOCKET', ''),
     'charset' => env('DB_CHARSET', 'utf8mb4'),
@@ -59,6 +59,10 @@ return [
     'prefix_indexes' => true,
     'strict' => true,
     'engine' => null,
+
+    'options' => extension_loaded('pdo_mysql') ? array_filter([
+        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+    ]) : [],
 ],
 
         'mariadb' => [
