c6987b4f64cf5a798fe3114110865da799a47193
Esha Minahil <eshaminahil1@gmail.com>
2026-06-16 19:09:56 +0500

    Fix TiDB SSL connection

--- config/database.php
+++ config/database.php
@@ -61,8 +61,8 @@ return [
     'engine' => null,
 
     'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-    ]) : [],
+    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+]) : [],
 ],
 
         'mariadb' => [
